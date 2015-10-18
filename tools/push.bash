a11078fe053f6644ae128c5da9f9581f86d58053
Casey Marshall <cmars@cmarstech.com>
2015-10-17 23:25:09 -0500

    Add subtree push script.

---
+++ tools/push.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+git subtree push -P src/github.com/cmars/ooclient git@github.com:cmars/ooclient.git master
+git subtree push -P src/github.com/cmars/oostore git@github.com:cmars/oostore.git master            
+git subtree push -P charms/layers/pgsql git@github.com:cmars/juju-relation-pgsql.git master            
+
