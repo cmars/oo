07f06055ae1993da1a8d4a3e5d7b742b8363a5bd
Casey Marshall <me@cmars.tech>
2015-12-05 22:02:47 -0600

    Also push to quorum repo.
    Clean up whitespace, add bash flags.

--- tools/push-oo.bash
+++ tools/push-oo.bash
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
 git subtree push -P src/github.com/cmars/ooclient git@github.com:cmars/ooclient.git master
-git subtree push -P src/github.com/cmars/oostore git@github.com:cmars/oostore.git master            
-git subtree push -P charms/layers/pgsql git@github.com:cmars/juju-relation-pgsql.git master            
+git subtree push -P src/github.com/cmars/oostore git@github.com:cmars/oostore.git master
+git subtree push -P src/github.com/cmars/quorum git@github.com:cmars/quorum.git master
+git subtree push -P charms/layers/pgsql git@github.com:cmars/juju-relation-pgsql.git master
 
